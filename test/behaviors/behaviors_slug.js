75e16807c9e4fce9d869428e454261f00ca66da7
arnaud <adoyen@perso.be>
2024-08-17 16:23:14 +0200

    Update for compatibility of Meteor 3.0 - publication

--- test/behaviors/behaviors_slug.js
+++ test/behaviors/behaviors_slug.js
@@ -5,8 +5,8 @@ Tinytest.add('Behaviors - Slug', function(test) {
   // Reset Astronomy.
   reset();
 
-  var SlugsA = new Mongo.Collection(null);
-  var SlugsB = new Mongo.Collection(null);
+  let SlugsA = new Mongo.Collection(null);
+  let SlugsB = new Mongo.Collection(null);
 
   SlugsA.find({}, {
     transform: null
@@ -20,7 +20,7 @@ Tinytest.add('Behaviors - Slug', function(test) {
     SlugsB.remove(slug._id);
   });
 
-  var SlugA = Class.create({
+  let SlugA = Class.create({
     name: 'SlugA',
     collection: SlugsA,
     fields: {
@@ -31,7 +31,7 @@ Tinytest.add('Behaviors - Slug', function(test) {
     }
   });
 
-  var SlugB = Class.create({
+  let SlugB = Class.create({
     name: 'SlugB',
     collection: SlugsB,
     fields: {
@@ -48,23 +48,23 @@ Tinytest.add('Behaviors - Slug', function(test) {
     }
   });
 
-  var diacritics = 'ąáàâãăåäāǟ' + 'ćĉčçċ' + 'ďđḑ' + 'ęéèêěĕëēė' + 'ģĝğġģ' +
+  let diacritics = 'ąáàâãăåäāǟ' + 'ćĉčçċ' + 'ďđḑ' + 'ęéèêěĕëēė' + 'ģĝğġģ' +
     'ĥħ' + 'íìîĭïīįĩı' + 'ĵ' + 'ķ' + 'łļĺľŀ' + 'ňńņñŉ' + 'óòôŏöōøőȯȱȭõ' +
     'řŗŕ' + 'śŝšş' + 'ťțŧ' + 'úùûūųüűŭũů' + 'ŵ' + 'ŷýÿ' + 'źżž' +
     'абчдеёэфгийяъюкхлмнопрсшщтцувызж';
-  var expected = 'aaaaaaaaaa' + 'ccccc' + 'ddd' + 'eeeeeeeee' + 'ggggg' +
+  let expected = 'aaaaaaaaaa' + 'ccccc' + 'ddd' + 'eeeeeeeee' + 'ggggg' +
     'hh' + 'iiiiiiiii' + 'j' + 'k' + 'lllll' + 'nnnnn' + 'oooooooooooo' +
     'rrr' + 'ssss' + 'ttt' + 'uuuuuuuuuu' + 'w' + 'yyy' + 'zzz' +
     'abchdeeefgiiiaieiukkhlmnoprsshshchttsuvyzzh';
 
-  var slugA1 = new SlugA();
+  let slugA1 = new SlugA();
   slugA1.set('name', 'Slug ' + diacritics);
   slugA1.save();
   test.equal(slugA1.get('slug'), 'slug-' + expected,
     'The slug function does not work properly'
   );
 
-  var slugA2 = new SlugA();
+  let slugA2 = new SlugA();
   slugA2.set('name', 'Slug ' + diacritics);
   slugA2.save();
   test.equal(slugA2.get('slug'), 'slug-' + expected + '-2',
@@ -77,13 +77,13 @@ Tinytest.add('Behaviors - Slug', function(test) {
     'It should not be possible to update a slug'
   );
 
-  var slugB1 = new SlugB();
+  let slugB1 = new SlugB();
   slugB1.set('title', 'Slug ' + diacritics);
   slugB1.save();
   test.isNotUndefined(slugB1.get('slugged'),
     'The name of a field for the slug storage should be "slugged"'
   );
-  test.isTrue(typeof slugB1.get('slugged') === 'string'),
+  test.isTrue(typeof slugB1.get('slugged') === 'string',
     'The slug should be created from the value of the "title" field'
   );
   test.isTrue(_contains(slugB1.get('slugged'), '_'),
@@ -96,7 +96,7 @@ Tinytest.add('Behaviors - Slug', function(test) {
     'It should be possible to update a slug"'
   );
 
-  slugB2 = new SlugB();
+  const slugB2 = new SlugB();
   slugB2.set('title', 'Slug2 ' + diacritics);
   slugB2.save();
   test.equal(slugB2.get('slugged'), 'slug2_' + expected,
